9e5104b57c5c57546d233c8bbe1cf35566814fe4
Faraz Shah <farazshah9095@gmail.com>
2024-09-18 04:29:56 -0700

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,32 +1,30 @@
-// alert('hi my name is faraz shah and i am learning js ')
-
-// for(let i =100; i==100; i++){
-//     console.log("i=",i);
-// }
-// let i =0;
-// while(i<=100){
-    
-// if(i%2 === 0){
-//     console.log("i=",i);
-//     document.write(i,"<br>");
-// }
-//     i++;
-// }
+ alert('hi my name is faraz shah and i am learning js ')
+ for(let i =100; i==100; i++){
+    console.log("i=",i);
+ }
+ let i =0;
+ while(i<=100){
+     if(i%2 === 0){
+    console.log("i=",i);
+    document.write(i,"<br>");
+ }
+     i++;
+ }
 
 
 
-// let gameNum=25;
+let gameNum=25;
 
-// let usernum= prompt("choose you number");
+let usernum= prompt("choose you number");
 
-// while(usernum != gameNum){
-//     usernum= prompt("choose error num choose again number");
-// }
-//     console.log("choose right num good luck ");
+while(usernum != gameNum){
+     usernum= prompt("choose error num choose again number");
+ }
+    console.log("choose right num good luck ");
 
 
 
-// let userName=prompt('enter your name');
+let userName=prompt('enter your name');
 
-// let output=`@${userName}${userName.length}`;
-// console.log(output);
+ let output=`@${userName}${userName.length}`;
+console.log(output);
