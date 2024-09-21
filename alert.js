c715246d9f2c11d00a65b13c99b9b1b8a5ea8af3
Faraz Shah <farazshah9095@gmail.com>
2024-09-21 10:06:30 -0700

    Update alert.js

--- alert.js
+++ alert.js
@@ -1,30 +1,105 @@
- alert('hi my name is faraz shah and i am learning js ')
- for(let i =100; i==100; i++){
-    console.log("i=",i);
- }
- let i =0;
- while(i<=100){
-     if(i%2 === 0){
-    console.log("i=",i);
-    document.write(i,"<br>");
- }
-     i++;
- }
+// alert('hi my name is faraz shah and i am learning js ')
+
+// for(let i =100; i==100; i++){
+//     console.log("i=",i);
+// }
+// let i =0;
+// while(i<=100){
+    
+// if(i%2 === 0){
+//     console.log("i=",i);
+//     document.write(i,"<br>");
+// }
+//     i++;
+// }
 
 
 
-let gameNum=25;
+// let gameNum=25;
 
-let usernum= prompt("choose you number");
+// let usernum= prompt("choose you number");
 
-while(usernum != gameNum){
-     usernum= prompt("choose error num choose again number");
- }
-    console.log("choose right num good luck ");
+// while(usernum != gameNum){
+//     usernum= prompt("choose error num choose again number");
+// }
+//     console.log("choose right num good luck ");
 
 
 
-let userName=prompt('enter your name');
+// let userName=prompt('enter your name');
 
- let output=`@${userName}${userName.length}`;
-console.log(output);
+// let output=`@${userName}${userName.length}`;
+// console.log(output);
+
+
+// item=[250,645,300,900,50];
+// let idx=0;
+// for(let val of item){
+// let offer=val /10;
+// // console.log(offer);
+// item[idx]=item[idx]-offer;
+//     console.log(item[idx]);
+//     idx++;
+// }
+
+
+
+// for loop
+
+// function test(a) {
+//     let count =0;
+//     let str=a;
+//     for(let val of str){
+//         if(val === "a" || val === "e" || val === "i" || val === "o" || val === "u"){
+            
+//             // return val;
+//             count++;
+//         console.log(count);
+//         }
+        
+//     }
+// }
+// console.log(test("apple"));
+
+
+// let test=(a)=>{
+//     let count =0;
+//     let str=a;
+//     for(let val of str){
+//         if(val === "a" || val === "e" || val === "i" || val === "o" || val === "u"){
+            
+            
+//             count++;
+            
+//         }
+        
+//     }
+//     return count;
+// }
+// console.log(test("apple"));
+
+
+// let arr = [90,95,98,25,4,155,44,99];
+
+//  let newarr=arr.filter((val)=>{
+// return val >= 90;
+//  })
+// console.log(newarr);
+
+// let usernum=prompt("enter the number");
+// arr=[];
+// for(let i=1;i<=usernum;i++){
+//      arr[i-1]=i;
+
+// }
+// // console.log(arr
+// let newarr=arr.reduce((res,curr)=>{
+//     return res+curr;
+
+// })
+// let array=arr.reduce((res,curr)=>{
+//     return res*curr;
+
+// })
+
+// console.log(array);
